0d8cb288bac5b2e82f12b122a06b69be1f63d09c
Yemiez <oskardahlberg1@gmail.com>
2021-08-08 13:05:21 +0200

    Correction of phrase

--- examples/array_replace.c
+++ examples/array_replace.c
@@ -15,7 +15,7 @@ int main()
 
 	printf("Before length: %d\n", cjson_array_length(arr));
 	int did_replace = cjson_replaceidx(arr, 0, cjson_create_string("1"), NULL);
-	printf("Did erase: %d\n", did_replace);
+	printf("Did replace: %d\n", did_replace);
 	printf("Before length: %d\n", cjson_array_length(arr));
 	
 	stringified = cjson_stringify(arr);
